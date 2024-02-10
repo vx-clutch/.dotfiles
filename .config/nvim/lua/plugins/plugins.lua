b6170844e934a51790d785df68469025a55b94cd
vx_clutch <owestness@gmail.com>
2024-02-10 15:24:02 -0500

    save

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -6,19 +6,18 @@ return {
 	'ThePrimeagen/harpoon',
 	'mbbill/undotree',
 	'tpope/vim-fugitive',
-	'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
-	'neovim/nvim-lspconfig',
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	'hrsh7th/cmp-nvim-lsp',
 	'hrsh7th/nvim-cmp',
 	'L3MON4D3/LuaSnip',
 	'williamboman/mason.nvim',
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+  
 	-- multi line imports
-	{{
-		'nvim-treesitter/nvim-treesitter', 
-		build = ':TSUpdate'
-	}}
-
-}
+	{
+	  'nvim-treesitter/nvim-treesitter',  
+	  build = ':TSUpdate'
+	}
+  }
+  
