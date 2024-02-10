2b7437d9f2249a40703fb3c2ffd03325032f09d5
vx_clutch <owestness@gmail.com>
2024-02-10 13:11:04 -0500

    save

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -5,10 +5,17 @@ return {
 	"nvim-telescope/telescope.nvim",
 	'ThePrimeagen/harpoon',
 	"mbbill/undotree",
+	"tpope/vim-fugitive",
+	'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+	'neovim/nvim-lspconfig',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/nvim-cmp',
+	'L3MON4D3/LuaSnip',
 
 	-- multi line imports
 	{{
 		"nvim-treesitter/nvim-treesitter", 
 		build = ":TSUpdate"
 	}}
+
 }
