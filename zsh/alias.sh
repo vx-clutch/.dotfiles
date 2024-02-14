56ed389995e9b5847745a3e19718c36f98a2f655
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2024-02-14 15:52:12 -0500

    Update alias.sh

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,12 +1,7 @@
 alias cd..='cd ..'
-alias fastping='ping -c 99 -s.2'
 alias ls='ls --color=auto'
-alias now='date +"%T"'
-alias nowdate='date +"%d-%m-%Y"'
 alias nowtime=now
 alias path='echo -e ${PATH//:/\\n}'
 alias ping='ping -c 4'
 alias zshconfig='nvim ~/.zshrc'
 alias ll='ls -lah'
-alias vim='nvim'
-alias cd="z"
