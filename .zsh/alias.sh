40d5461f31e4bf0fc6e575cc870f6fb444357412
vx_clutch <owestness@gmail.com>
2024-02-21 16:21:26 -0500

    Changed aliases

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -1,9 +1,4 @@
-alias cd..='cd ..'
 alias ls='ls --color=auto'
-alias nowtime=now
-alias path='echo -e ${PATH//:/\\n}'
 alias ping='ping -c 4'
-alias zshconfig='nvim ~/.zshrc'
 alias ll='ls -lah'
-alias quicksave='git add .; git commit -m "save";git push'
 alias nf='neofetch'
