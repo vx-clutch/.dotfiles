fd9277ee47efb145d3377c7d21f258b19bdf0968
vx_clutch <owestness@gmail.com>
2024-03-20 17:59:47 -0400

    save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -58,21 +58,6 @@ return {
          }
      })
 
-     -- `:` cmdline setup.
-     cmp.setup.cmdline(':', {
-         mapping = cmp.mapping.preset.cmdline(),
-         sources = cmp.config.sources({
-             { name = 'path' }
-         }, {
-             {
-                 name = 'cmdline',
-                 option = {
-                     ignore_cmds = { 'Man', '!' }
-                 }
-             }
-         })
-     })
-
      cmp.setup({
          sources = {
              { name = 'nvim_lsp' },
